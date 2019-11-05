3ee07b54c158c5a6e38e885e5de3ed696c094be2
wakeupmh <wakeupmh@gmail.com>
2019-11-05 17:13:04 -0300

    appending pages on feed

--- instaclone/src/pages/Feed/index.js
+++ instaclone/src/pages/Feed/index.js
@@ -5,19 +5,20 @@ import { Post } from './styles'
 
 export default function Feed() {
     const [feed, setFeed] = useState([]);
-    
+    const [page, setPage] = useState(1);
+
+    async function loadPage(pageNumber = page){
+        const response = await fetch(
+            `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
+        );
+        const data = await response.json();
+
+        setFeed([...feed, ...data]);
+        setPage(pageNumber + 1);
+    }
+
     useEffect(()=>{
-        async function loadFeed() {
-            const response = await fetch(
-                'http://localhost:3000/feed?_expand=author&_limit=5&_page=1'
-            );
-
-            const data = await response.json();
-
-            setFeed(data);
-        }
-
-        loadFeed();
+        loadPage();
     }, []); 
 
     return(
@@ -25,6 +26,8 @@ export default function Feed() {
             <FlatList 
                 data={feed}
                 keyExtractor={post => String(post.id)}
+                onEndReached={() => loadPage()}
+                onEndReachedThreshold={0.1}
                 renderItem={({ item })=> (
                     <Post>
                         <Header>
@@ -40,7 +43,6 @@ export default function Feed() {
                     </Post>
                 )}
             />
-
         </View>
     )
 }
